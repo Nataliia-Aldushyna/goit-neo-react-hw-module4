1642eeef82babe7860922c61fbe5899d5e8abf64
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2024-12-20 22:50:17 +0100

    update api.js

--- services/api.js
+++ services/api.js
@@ -1,13 +1,13 @@
-import api from "./api";
+// import api from "./api";
 
-export async function searchImage(query, page) {
-  const url = "/search/photos";
-  const { data } = await api.get(url, {
-    params: {
-      query,
-      page,
-      per_page: 12,
-    },
-  });
-  return data;
-}
+// export async function searchImage(query, page) {
+//   const url = "/search/photos";
+//   const { data } = await api.get(url, {
+//     params: {
+//       query,
+//       page,
+//       per_page: 12,
+//     },
+//   });
+//   return data;
+// }
